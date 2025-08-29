6b2e250f7595af9935855ef9c4ae90ff3aa1e132
Antonia90 <antominnucci@gmail.com>
2025-08-29 22:12:31 +0200

    Add models and migrations

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    protected $fillable = ['ingredient_type', 'name'];
+
+    public function recipes()
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_ingredient')
+            ->withPivot('unit', 'quantity_per_serving');
+    }
+}
---
+++ app/Models/Recipe.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    protected $fillable = ['diet_category', 'name', 'description', 'base_servings'];
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'recipe_ingredient')
+            ->withPivot('unit', 'quantity_per_serving');
+    }
+}
